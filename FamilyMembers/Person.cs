183eb1205b95c3f848255132587a06fd6cf8ed8e
Boichuk Julia <juliaboichuk503@gmail.com>
2023-11-28 22:05:12 +0300

    Create project

---
+++ FamilyMembers/Person.cs
@@ -0,0 +1,147 @@
+﻿using System.Linq;
+
+namespace FamilyMembers
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly Birthday { get; set; }
+        public Gender Gender { get; set; }
+        private Person? mother;
+        private Person? father;
+        public Person? Mother
+        {
+            get { return mother; }
+
+            set
+            {
+                mother = value;
+                value?.Children.Add(this);
+            }
+        }
+        public Person? Father
+        {
+            get { return father; }
+
+            set
+            {
+                father = value;
+                value?.Children.Add(this);
+            }
+        }
+
+        public Person? Partner { get; set; }
+
+        public List<Person> Children { get; set; }
+
+        public Person(string name, string surname, DateOnly birthday, Gender gender)
+        {
+            Name = name;
+            Surname = surname;
+            Birthday = birthday;
+            Gender = gender;
+            Children = new List<Person>();
+        }
+
+        public Person[] GetParents()
+        {
+            return new Person[] { Mother, Father };
+        }
+
+        public Person[] GetGrandmothers()
+        {
+            return new Person[] { Mother?.Mother, Father?.Mother };
+        }
+
+        public Person[] GetGrandfathers()
+        {
+            return new Person[] { Mother?.Father, Father?.Father };
+        }
+
+        public List<Person> GetBrothers(Person person)
+        {
+            List<Person> brothers = new List<Person>();
+            Person[] parents = person.GetParents();
+            foreach (Person parent in parents)
+            {
+                if (parent != null)
+                {
+                    brothers.AddRange(from Person child in parent.Children
+                                      select child);
+                }
+            }
+
+            brothers = brothers.Distinct().ToList();
+            brothers.Remove(this);
+            return brothers;
+        }
+
+        public List<Person> GetAllBrothers()
+        {
+            List<Person> brothers = new List<Person>();
+            List<Person> parentBrothers = new List<Person>();
+            brothers.AddRange(GetBrothers(this));
+            if (Mother != null) parentBrothers.AddRange(GetBrothers(Mother));
+            if (Father != null) parentBrothers.AddRange(GetBrothers(Father));
+            foreach (Person person in parentBrothers)
+            {
+                if (person != null)
+                {
+                    brothers.AddRange(from Person child in person.Children
+                                      select child);
+                }
+            }
+            brothers = brothers.Distinct().ToList();
+            brothers.Remove(this);
+            return brothers;
+        }
+
+
+        public string GetFullName()
+        {
+            return Name + " " + Surname;
+        }
+
+        public void PrintInfoAboutPerson()
+        {
+            Console.WriteLine("Родители:");
+            Print(GetParents());
+
+            Console.WriteLine("Муж/жена: ");
+            Console.WriteLine(Partner == null ? "--" : Partner.GetFullName());
+
+            Console.WriteLine("Бабушки:");
+            Print(GetGrandmothers());
+
+            Console.WriteLine("Дедушки:");
+            Print(GetGrandfathers());
+
+            Console.WriteLine("Дети:");
+            Print(Children);
+
+            Console.WriteLine("Братья/сестры:");
+            Print(GetAllBrothers());
+        }
+
+        public void Print(Person[] persons)
+        {
+            foreach (Person person in persons)
+            {
+                if (person == null) Console.WriteLine("--");
+                else Console.WriteLine(person.GetFullName());
+            }
+        }
+        public void Print(List<Person> persons)
+        {
+            if (persons == null || persons.Count == 0) { Console.WriteLine("--"); }
+            else
+            {
+                foreach (Person person in persons)
+                {
+                    if (person != null) Console.WriteLine(person.GetFullName());
+                }
+            }
+        }
+    }
+}
---
+++ FamilyMembers/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace FamilyMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person firstGrandmother = new Person(
+                "Ольга", "Дихтярь", new DateOnly(1960, 1, 1), Gender.woman
+            );
+            Person secondGrandmother = new Person(
+                "Надежда", "Бойчук", new DateOnly(1961, 2, 2), Gender.woman
+            );
+
+            Person firstGrandfather = new Person(
+                "Василий", "Дихтярь", new DateOnly(1962, 3, 3), Gender.man
+            );
+            Person secondGrandfather = new Person(
+                "Валерий", "Бойчук", new DateOnly(1963, 4, 4), Gender.man
+            );
+            firstGrandmother.Partner = firstGrandfather;
+            secondGrandmother.Partner = secondGrandfather;
+            firstGrandfather.Partner = firstGrandmother;
+            secondGrandfather.Partner = secondGrandmother;
+
+            Person mother = new Person(
+                "Юлия", "Бойчук", new DateOnly(1980, 5, 5), Gender.woman
+            );
+            Person father = new Person(
+                "Иван", "Дихтярь", new DateOnly(1981, 6, 6), Gender.man
+            );
+            mother.Mother = secondGrandmother;
+            mother.Father = secondGrandfather;
+            mother.Partner = father;
+
+            father.Mother = firstGrandmother;
+            father.Father = firstGrandfather;
+            father.Partner = mother;
+
+
+            Person firstChild = new Person(
+                "Полина", "Дихтярь", new DateOnly(2000, 7, 7), Gender.woman
+            );
+            Person secondChild = new Person(
+                "Сергей", "Дихтярь", new DateOnly(2001, 8, 8), Gender.man
+            );
+            firstChild.Mother = mother;
+            firstChild.Father = father;
+            secondChild.Mother = mother;
+            secondChild.Father = father;
+
+            Person motherSister = new Person(
+                "Татьяна", "Дихтярь", new DateOnly(1877, 9, 9), Gender.woman
+            );
+            Person motherSisterChild = new Person(
+                "Давид", "Дихтярь", new DateOnly(2001, 10, 10), Gender.man
+            );
+            motherSister.Mother = firstGrandmother;
+            motherSister.Father = firstGrandfather;
+            motherSisterChild.Mother = motherSister;
+
+            Console.WriteLine("Первая бабушка: " + firstGrandmother.GetFullName());
+            firstGrandmother.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Вторая бабушка: " + secondGrandmother.GetFullName());
+            secondGrandmother.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Первый дедушка: " + firstGrandfather.GetFullName());
+            firstGrandfather.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Второй  дедушка: " + secondGrandfather.GetFullName());
+            secondGrandfather.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Мама: " + mother.GetFullName());
+            mother.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Папа: " + father.GetFullName());
+            father.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Ребенок 1: " + firstChild.GetFullName());
+            firstChild.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Ребенок 2: " + secondChild.GetFullName());
+            secondChild.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Сестра мамы: " + motherSister.GetFullName());
+            motherSister.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+            Console.WriteLine("Двоюродный брат ребенка: " + motherSisterChild.GetFullName());
+            motherSisterChild.PrintInfoAboutPerson();
+            Console.WriteLine();
+
+        }
+        
+    }
+}
